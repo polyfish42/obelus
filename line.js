96772d6c47af61fa141528c108a801ed915cce5e
Jacob Brady <polyfish42@gmail.com>
2018-03-21 18:20:51 -0400

    Continuous, reversible lines

--- line.js
+++ line.js
@@ -19,35 +19,71 @@ function clickHandler(e) {
 
 function mouseMoveHandler(e) {
   const dx = lastMouseCoords[0] - e.screenX
-  // const dy = lastMouseCoords[1] - e.screenY
+  const dy = lastMouseCoords[1] - e.screenY
   lastMouseCoords = [e.screenX, e.screenY]
-  addLineSegment(dx)
+  addLineSegment(dx, dy)
 }
 
 function addLineSegment(dx, dy) {
   let last = lineSegments[segmentIndex - 1]
   let x1 = last[0]
   let y1 = last[1]
-  lineSegments[segmentIndex++] = [x1 - dx, y1]
+  let newSegment = [x1 - dx, y1 - dy]
+
+  if (dx > 0) {
+    for (var i = 0; i < dx - 1; i++) {
+      segmentIndex--
+    }
+  } else if (dx < 0 && notInBoxBounds(x1 - dx, y1 - dy)) {
+    for (var i = dx; i < 0; i++) {
+      lineSegments[segmentIndex++] = [x1 - dx, y1]
+    }
+  }
 }
 
 function drawLine() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // let x1 = lineSegments[0][0]
+    // let y1 = lineSegments[0][1]
+    // let x2 = lineSegments[segmentIndex - 1][0]
+    // let y2 = lineSegments[segmentIndex - 1][1]
+
+    // ctx.beginPath()
+    // ctx.moveTo(x1, y1)
+    // ctx.lineWidth = 15;
+    // ctx.lineTo(x2, y2)
+    // ctx.stroke()
+
     let x1 = lineSegments[0][0]
     let y1 = lineSegments[0][1]
-    let x2 = lineSegments[segmentIndex - 1][0]
-    let y2 = lineSegments[segmentIndex - 1][1]
 
-    ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineWidth = 15;
-    ctx.lineTo(x2, y2)
-    ctx.stroke()
+    for (var i = 0; i < segmentIndex - 1; i++) {
+      let x2 = lineSegments[i + 1][0] || x1
+      let y2 = lineSegments[i + 1][1] || y1
 
+      ctx.beginPath()
+      ctx.moveTo(x1, y1)
+      ctx.lineWidth = 15;
+      ctx.lineTo(x2, y2)
+      ctx.stroke()
+
+      x1 = lineSegments[i + 1][0]
+      y1 = lineSegments[i + 1][1]
+    }
 }
 
 function drawBox() {
-  
+  ctx.beginPath()
+  ctx.rect(20, 35, 200, 200)
+  ctx.stroke()
+}
+
+function notInBoxBounds(x, y) {
+  if (x < 220 && x > 20 && y > 35 && y < 235) {
+    return false
+  } else {
+    return true
+  }
 }
 
 function draw() {
